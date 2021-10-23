2d62f39f3868d3b88a318066596fca044bce789e
kwamekert <kwamekert@gmail.com>
2021-10-23 01:06:13 +0000

    build configuration updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  countryApi: 'https://restcountries.com/v2/all',
+  numVerifyApi: 'https://apilayer.net/api',
+  numVerifyApiKey: 'eb588dbf70cb81df1c8d374269db9d18',
 };
